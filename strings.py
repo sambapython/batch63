6ff7970a5f942fe5dd82a9fa11cb88fa38e6d896
sambapython <sambapython@gmail.com>
2019-07-31 10:09:13 +0530

    2019-07-31 strings.

---
+++ strings.py
@@ -0,0 +1,34 @@
+s="python program"
+print(s[0])
+print(s[13])
+print(s[14])
+
+print(s[-1])
+print(s[-10])
+print(s[-14])
+print(s[-15])
+
+print(s[0]+s[1]+s[2])
+print(s[0:3])
+print(s[5:13])
+print(s[0:10])
+print(s[0:10:2])
+
+print(s[0:10])
+print(s[10:0])
+s="python program"
+print(s[-10:-4])
+print(s[-10:10])
+
+print(s[10:5])
+print(s[10:5:-1])
+
+s="python program"
+print(s[10:5:-1])
+print(s[10:5:-1])
+
+
+
+s="python program"
+print(s[10:5])
+print(s[-10:-5])
