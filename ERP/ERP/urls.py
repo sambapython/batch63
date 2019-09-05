5c8bcb8a22b74a069cf71c07d4133bf46168239b
sambapython@gmail.com <sambapython@gmail.com>
2019-09-05 10:08:29 +0530

    2019-09-5 final commit

--- ERP/ERP/urls.py
+++ ERP/ERP/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sales.views import cust_fun
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("customers/",cust_fun)
 ]
--- ERP/sales/views.py
+++ ERP/sales/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from sales.models import Customer
 
 # Create your views here.
+'''
+def cust_fun(request):
+	cust_data = Customer.objects.all()
+	cust_names = [i.name for i in cust_data]
+
+	return HttpResponse(cust_names)
+'''
+def cust_fun(request):
+	cust_data = Customer.objects.all()
+	cust_names = [i.name for i in cust_data]
+	return render(request,"cust.html",{"customers":cust_names})
